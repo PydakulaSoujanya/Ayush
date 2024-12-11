3dfacbadbc14771597304abc942a764aedb4ef66
PydakulaSoujanya <141412830+PydakulaSoujanya@users.noreply.github.com>
2024-12-11 16:17:06 +0530

    Added team folder

---
+++ Team-general/team-docs-links.php
@@ -0,0 +1,10 @@
+<h3>Project links</h3>
+
+<br>
+<br>
+<a href="https://drive.google.com/drive/folders/1IquPBFIE9A_qGuwvi-8dnKOwH5_Zcybn" target="_blank">Ayush docs </a>
+
+<br>
+<br>
+<a href="https://docs.google.com/spreadsheets/d/1AZR5lDSNv_LcFXpjnBDSkagTOnTgA9XApdGI1f1223E/edit?usp=sharing" target="_blank">Ayush test sheet</a>
+
